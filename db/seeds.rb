bba3f00cad06cccd0bf660ef2a4c469b4644daae
Fabian Kedzierski <grafska@gmail.com>
2017-08-14 12:41:48 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
-# seeds
+User.create(name: 'Admin', email: 'admin@naibaf.pl', password: 'adminadmin2017', roles_mask: 1)
+
+Warehouse.create(
+  name: 'My first Warehouse',
+  description: 'Change the name of this Warehouse and add a better description :)'
+)
+puts 'Seeds has been imported successfully.'
